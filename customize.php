51acc1c77d9e4b29a727cb887a4feb6447f2fe04
Mohammad Fathi <mimfapost@gmail.com>
2025-04-04 19:51:18 +0330

    v2.0.0 stable

--- customize.php
+++ customize.php
@@ -4,14 +4,14 @@
     array("Name" => "EXERCISE", "Path" => "/exercise", "Image" => "calendar"),
     array(
         "Name" => "COURSE",
-        "Path" => "/cat/Course",
+        "Path" => \_::$Address->CategoryRoute."Course",
         "Image" => "book",
         "Items" =>
             array(
-                array("Name" => "Windows Forms App Development", "Path" => "/cat/Course/Windows Forms App Development"),
-                array("Name" => "SQL Tutorial", "Path" => "/cat/Course/SQL Tutorial"),
-                array("Name" => "Intensive Web Development", "Path" => "/cat/Course/Intensive Web Development"),
-                array("Name" => "Other Tutorials", "Path" => "/cat/Media/Doc")
+                array("Name" => "Windows Forms App Development", "Path" => \_::$Address->CategoryRoute."Course/Windows Forms App Development"),
+                array("Name" => "SQL Tutorial", "Path" => \_::$Address->CategoryRoute."Course/SQL Tutorial"),
+                array("Name" => "Intensive Web Development", "Path" => \_::$Address->CategoryRoute."Course/Intensive Web Development"),
+                array("Name" => "Other Tutorials", "Path" => \_::$Address->CategoryRoute."Media/Doc")
             )
     ),
     array(
@@ -28,7 +28,7 @@
 
 \_::$Info->Surtcuts = array(
     array("Name" => "MENU", "Path" => "viewSideMenu()", "Image" => "bars"),
-    array("Name" => "COURSE", "Path" => "/cat/Course", "Image" => "book"),
+    array("Name" => "COURSE", "Path" => \_::$Address->CategoryRoute."Course", "Image" => "book"),
     array("Name" => "HOME", "Path" => "/", "Image" => "home"),
     array("Name" => "EXERCISE", "Path" => "/exercise", "Image" => "calendar"),
     array("Name" => "CONTACT", "Path" => "/contact", "Image" => "address-book")

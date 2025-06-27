97fd3b4297b8ce151423720d00b07c809f69533f
Mohammad Fathi <mimfapost@gmail.com>
2025-06-27 17:31:28 +0330

    V3.0.0

--- customize.php
+++ customize.php
@@ -1,17 +1,11 @@
 <?php
 $menus = [
-    array("Name" => "EXERCISE", "Path" => "/exercises", "Image" => "calendar"),
-    array(
-        "Name" => "COURSE",
+    array("Name" => "EXERCISES", "Path" => "/exercises", "Image" => "calendar"),
+    "Course"=>array(
+        "Name" => "COURSES",
         "Path" => \_::$Address->CategoryRoute."Course",
         "Image" => "book",
-        "Items" =>
-            array(
-                array("Name" => "Windows Forms App Development", "Path" => \_::$Address->CategoryRoute."Course/Windows Forms App Development"),
-                array("Name" => "SQL Tutorial", "Path" => \_::$Address->CategoryRoute."Course/SQL Tutorial"),
-                array("Name" => "Intensive Web Development", "Path" => \_::$Address->CategoryRoute."Course/Intensive Web Development"),
-                array("Name" => "Other Tutorials", "Path" => \_::$Address->CategoryRoute."Media/Doc")
-            )
+        "Items" => []
     ),
     array(
         "Name" => "CONTACTS",
@@ -24,13 +18,19 @@ $menus = [
         )
     )
 ];
+$courses = [];
+foreach(array_slice(compute("category/all", ["Direction"=>"Course"]), 1) as $item)
+    $courses[] = ["Name"=>$item["Title"], "Path"=>\_::$Address->CategoryRoute."Course/".$item["Name"]];
+$menus["Course"]["Items"] = $courses;
+
 \_::$Info->MainMenus = [...\_::$Info->MainMenus,...$menus];
 \_::$Info->SideMenus = [...\_::$Info->SideMenus,...$menus];
 \_::$Info->Shortcuts = [
     array("Name" => "MENU", "Path" => "viewSideMenu()", "Image" => "bars"),
-    array("Name" => "COURSE", "Path" => \_::$Address->CategoryRoute."Course", "Image" => "book"),
+    array("Name" => "COURSES", "Path" => \_::$Address->CategoryRoute."Course", "Image" => "book"),
     array("Name" => "HOME", "Path" => \_::$Info->HomePath, "Image" => "home"),
-    array("Name" => "EXERCISE", "Path" => "/exercises", "Image" => "calendar"),
-    array("Name" => "CONTACT", "Path" => "/contact", "Image" => "envelope")
+    array("Name" => "EXERCISES", "Path" => "/exercises", "Image" => "calendar"),
+    array("Name" => "CONTACTS", "Path" => "/contact", "Image" => "envelope")
 ];
+
 ?>

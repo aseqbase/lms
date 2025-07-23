4cdb10e6be38557562757a910caf821e501c7044
Mohammad Fathi <mimfapost@gmail.com>
2025-07-23 21:30:52 +0330

    v3.3.0

--- router.php
+++ router.php
@@ -1,17 +1,17 @@
 <?php
 // To unset the default router sat at the bottom layers
-\_::$Back->Router->Route->Reset();
+\_::$Back->Router->On()->Reset();
 
 /**
  * Use your routers by below formats
- * \_::$Back->Router->Route("A Part Of Path?")->Default("Route Name");
+ * \_::$Back->Router->On("A Part Of Path?")->Default("Route Name");
  */
-\_::$Back->Router->Route("exercises")->Default("exercises");
-\_::$Back->Router->Route("exercise")->Default("exercise");
-\_::$Back->Router->Route("$|home")
+\_::$Back->Router->On("exercises")->Default("exercises");
+\_::$Back->Router->On("exercise")->Default("exercise");
+\_::$Back->Router->On("$|home")
     ->if(auth(\_::$Config->UserAccess))->Default(fn()=>view("part",["Name"=>"sign/dashboard"]))
-    ->else->Default(fn()=>view("part", ["Name"=>"sign/in"]));
+    ->else()->Default(fn()=>view("part", ["Name"=>"sign/in"]));
 
 // To route other requests to the DefaultRouteName
-\_::$Back->Router->Route->Default(\_::$Config->DefaultRouteName);
+\_::$Back->Router->On()->Default(\_::$Config->DefaultRouteName);
 ?>

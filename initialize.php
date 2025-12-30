96514f93f314dab48df5cca57e89f1800a3db893
Mohammad Fathi <mimfapost@gmail.com>
2025-12-30 17:27:40 +0330

    Simplifing Simplify initialization and consolidate configuration/information into Back and Front objects. Deprecate separate route/config/info files and introduce a unified initialize/customize flow.

--- initialize.php
+++ initialize.php
@@ -23,12 +23,25 @@ foreach(array_slice(compute("category/all", ["Direction"=>"Course"]), 1) as $ite
     $courses[] = ["Name"=>$item["Title"], "Path"=>\_::$Address->CategoryRoot."Course/".$item["Name"]];
 $menus["Course"]["Items"] = $courses;
 
-\_::$Info->MainMenus = [...\_::$Info->MainMenus,...$menus];
-\_::$Info->SideMenus = [...\_::$Info->SideMenus,...$menus];
-\_::$Info->Shortcuts = [
+\_::$Front->MainMenus = [...\_::$Front->MainMenus,...$menus];
+\_::$Front->SideMenus = [...\_::$Front->SideMenus,...$menus];
+\_::$Front->Shortcuts = [
     array("Name" => "MENU", "Path" => "viewSideMenu()", "Image" => "bars"),
     array("Name" => "COURSES", "Path" => \_::$Address->CategoryRoot."Course", "Image" => "book"),
-    array("Name" => "HOME", "Path" => \_::$Info->HomePath, "Image" => "home"),
+    array("Name" => "HOME", "Path" => \_::$Front->HomePath, "Image" => "home"),
     array("Name" => "EXERCISES", "Path" => "/exercises", "Image" => "calendar"),
     array("Name" => "CONTACTS", "Path" => "/contact", "Image" => "envelope")
 ];
+
+// To unset the default router sat at the bottom layers
+\_::$Router->On()->Reset();
+
+/**
+ * Use your routers by below formats
+ * \_::$Router->On("A Part Of Path?")->Default("Route Name");
+ */
+\_::$Router->On("exercises")->Default("exercises");
+\_::$Router->On("exercise")->Default("exercise");
+\_::$Router->On("$|home")
+    ->if(\_::$User->HasAccess(\_::$User->UserAccess))->Default(fn()=>view("part",["Name"=>"sign/dashboard"]))
+    ->else()->Default(fn()=>view("part", ["Name"=>"sign/in"]));
